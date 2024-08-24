4576e6fcaee2578e1d4ca8a45a9a4d2af14a214c
YousukeSatou <y.freestyle@gmail.com>
2024-08-24 16:28:10 +0900

    update

---
+++ my-project-next/app/members/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { getMemberList } from "../_libs/microcms";
+import styles from "./page.module.css";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
+
+export default async function Page() {
+  const data = await getMemberList({ limit: MEMBERS_LIST_LIMIT });
+  return (
+    <div className={styles.container}>
+      {data.contents.length === 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません。</p>
+      ) : (
+        <ul>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image.url}
+                alt=""
+                width={member.image.width}
+                height={member.image.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dt className={styles.position}>{member.position}</dt>
+                <dt className={styles.profile}>{member.profile}</dt>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
